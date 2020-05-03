0f7882df8a3dc550ed1d9a0da59038d38b726272
Edwin Yu <theedwinyu888@gmail.com>
2020-05-02 22:51:48 -0400

    more frontend

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -3,8 +3,16 @@ import React, { Component } from 'react';
 import { withRouter } from "react-router-dom";
 import io from 'socket.io-client';
 import ml5 from 'ml5';
+import { Button } from 'antd';
 
 class Room extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            socket: null,
+        };
+    }
 
     componentDidMount(){
         const {
@@ -21,10 +29,38 @@ class Room extends Component {
         socket.emit("joinRoom", roomId, name, email, isNewRoom);
     }
 
+    nextStudent = (e) => {
+        
+    }
+
+    startGraduation = (e) => {
+
+    }
+
     render(){
+        const {
+            studentId,
+            universityName,
+            classOf,
+            email,
+            roomId,
+            name,
+            isNewRoom
+        } = this.props.location.state;
         console.log(this.props.location.state);
         return (
+            <div>
             <h1>Hello</h1>
+
+            <Button key="submit" type="default" onClick={this.startGraduation} style={{color:'white', backgroundColor:'#002A52'}}>
+                Start Graduation
+            </Button>
+
+            <Button key="submit" type="default" onClick={this.nextStudent} style={{color:'white', backgroundColor:'#002A52'}}>
+                Next Student
+            </Button>
+
+            </div>
         )
     }
 
