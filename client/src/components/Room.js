51c237bc85260c896e22e91bd0e72d2cd3a7ea9f
Edwin Yu <theedwinyu888@gmail.com>
2020-05-03 08:53:04 -0400

    sentiment

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { withRouter } from "react-router-dom";
 import io from 'socket.io-client';
 import ml5 from 'ml5';
-import { Button, Row, Col } from 'antd';
+import { Button, Row, Col, Alert } from 'antd';
 import Audio from './Audio';
 import Chatroom from './Chatroom';
 import Logo from '../assets/logo.png'
@@ -83,65 +83,65 @@ class Room extends Component {
 
         })
 
-        if (navigator.mediaDevices.getUserMedia) {
-            navigator.mediaDevices.getUserMedia({ video: true })
-                .then(function (stream) {
-                let video = document.getElementById('video')
-                video.srcObject = stream;
-                video.play();
-                })
-                .catch(function (error) {
-                console.log(error);
-            });
-        }
+        // if (navigator.mediaDevices.getUserMedia) {
+        //     navigator.mediaDevices.getUserMedia({ video: true })
+        //         .then(function (stream) {
+        //         let video = document.getElementById('video')
+        //         video.srcObject = stream;
+        //         video.play();
+        //         })
+        //         .catch(function (error) {
+        //         console.log(error);
+        //     });
+        // }
 
-        const options = {
-            multiplier:0.50,
-            detectionType:"single",
-            imageScaleFactor:0.2
-        }
-        const poseNet = ml5.poseNet(document.getElementById('video'),options, modelLoaded);
-        function modelLoaded() {
-            // console.log('Model Loaded!');
-        }
-        poseNet.on('pose', (poses) => {
-            if(poses){
-                if(isFocused && handsPresent(poses) && !recieved){
-                    recieved = true
-                    console.log("fuck")
-                    socket.emit("getDiploma",roomId)
-                    setTimeout(()=>{
-                        socket.emit("processPerson",this.props.location.state.roomId)
-                    },5000)
-                }
-            }
+        // const options = {
+        //     multiplier:0.50,
+        //     detectionType:"single",
+        //     imageScaleFactor:0.2
+        // }
+        // const poseNet = ml5.poseNet(document.getElementById('video'),options, modelLoaded);
+        // function modelLoaded() {
+        //     // console.log('Model Loaded!');
+        // }
+        // poseNet.on('pose', (poses) => {
+        //     if(poses){
+        //         if(isFocused && handsPresent(poses) && !recieved){
+        //             recieved = true
+        //             console.log("fuck")
+        //             socket.emit("getDiploma",roomId)
+        //             setTimeout(()=>{
+        //                 socket.emit("processPerson",this.props.location.state.roomId)
+        //             },5000)
+        //         }
+        //     }
         
-        });
+        // });
 
-        let intervalID = window.setInterval(
-            ()=>{
-                let video = document.getElementById('video')
-                let canvas = document.getElementById('canvas')
-                let ctx = canvas.getContext('2d')
-                let shortside = Math.min(video.videoWidth,video.videoHeight)
-                let ydiff = (video.videoHeight - shortside)/2
-                let xdiff = (video.videoWidth - shortside)/2
-                ctx.drawImage(video,xdiff,ydiff,shortside,shortside,
-                    0,0,canvas.width,canvas.height)
-                if(isFocused){
-                    socket.emit("sendVideoFrames",roomId,canvas.toDataURL())
-                }
+        // let intervalID = window.setInterval(
+        //     ()=>{
+        //         let video = document.getElementById('video')
+        //         let canvas = document.getElementById('canvas')
+        //         let ctx = canvas.getContext('2d')
+        //         let shortside = Math.min(video.videoWidth,video.videoHeight)
+        //         let ydiff = (video.videoHeight - shortside)/2
+        //         let xdiff = (video.videoWidth - shortside)/2
+        //         ctx.drawImage(video,xdiff,ydiff,shortside,shortside,
+        //             0,0,canvas.width,canvas.height)
+        //         if(isFocused){
+        //             socket.emit("sendVideoFrames",roomId,canvas.toDataURL())
+        //         }
 
-            },1000/FPS)
+        //     },1000/FPS)
 
-        function handsPresent(results){
-            let sensitivity = 0.5
-            return (results[0].pose.leftWrist.confidence > sensitivity || results[0].pose.rightWrist.confidence > sensitivity)
-        }
+        // function handsPresent(results){
+        //     let sensitivity = 0.5
+        //     return (results[0].pose.leftWrist.confidence > sensitivity || results[0].pose.rightWrist.confidence > sensitivity)
+        // }
         
-        this.setState({
-            socket,
-        })
+        // this.setState({
+        //     socket,
+        // })
     }
 
 
